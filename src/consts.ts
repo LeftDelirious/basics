f3bdd83a819908dd010a68cd6a90944805337b53
Lirio Floyd <lirio.floyd@gmail.com>
2026-05-24 06:32:04 -0700

    cust

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Furry Soulmates";
+export const SITE_DESCRIPTION = "Adoptable dogs in Las Vegas";
